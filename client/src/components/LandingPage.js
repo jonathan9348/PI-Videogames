27ef6f1a9a513de4d817c4838fa0d506414c4554
unknown <jonathan.moran9348@gmail.com>
2022-09-28 02:12:00 -0300

    Last commit

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -8,12 +8,28 @@ import '../components/styles/LandingPage.css'
 
 export default function LandingPage() {
   return (
-    <div className='cont-img'>
+    <div className='cont-img'> 
+
+        <div className='cont-txt'>
+          <h1>Welcome players</h1>
+        </div>
+        
         <img src={LandingImage} alt='landing' className='img-land'/>
         
             <Link to = '/home'>
+                
                 <button type= 'submit' className='button_home'>PRESS START</button>
+           
             </Link>
+
+
+            <div className='cont-myName'>
+                <h3>By Jonathan Moran</h3>
+            </div>
+
+
+        
+            
         
     </div>
   )
